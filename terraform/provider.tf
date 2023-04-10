e48a081f73653b603949ed91b91035338c3af518
sandesh <sandesh@sandesh.myguest.virtualbox.org>
2023-04-10 19:36:24 +0530

    Terraform

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,7 +1,6 @@
 provider "aws" {
   region     = "${var.region}"
   version    = "v2.70.0"
-  access_key = "access_key"
-  secret_key = "secret_key"
-}
+  access_key = AWS_ACCESS_KEY_ID
+  secret_key = AWS_SECRET_ACCESS_KEY
 
