08369b8020926313886b01995bf8b8a11ff5ac53
sandesh <sandesh@sandesh.myguest.virtualbox.org>
2023-04-11 00:21:10 +0530

    key

--- terraform/config.tf
+++ terraform/config.tf
@@ -3,5 +3,7 @@ terraform {
     bucket = "sandeshterraform"
     key    = "provider.tf"
     region = "us-east-1"
-  }
+	  access_key = AWS_ACCESS_KEY_ID
+  secret_key = AWS_SECRET_ACCESS_KEY 
+ }
 }
