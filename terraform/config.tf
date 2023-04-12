d97ffc1c6add611d19a6deab16733c4fd23cea4f
sandesh <sandesh@sandesh.myguest.virtualbox.org>
2023-04-12 08:09:25 +0530

    Terraform

--- terraform/config.tf
+++ terraform/config.tf
@@ -1,7 +1,7 @@
-terraform {
-  backend "s3" {
-    bucket = "sandeshterraform"
-    key    = "provider.tf"
-    region = "us-east-1"
- }
-}
+#terraform {
+#  backend "s3" {
+#    bucket = "sandeshterraform"
+#    key    = "provider.tf"
+#    region = "us-east-1"
+# }
+#}
